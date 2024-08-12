8577108c3c34b1e00c51d6a07f788511e2dff6a2
Spy230 <121127523+Spy230@users.noreply.github.com>
2024-08-12 14:31:46 +0300

    редактирование информации о курьере.cs

--- regard/infoVendors.cs
+++ regard/infoVendors.cs
@@ -7,7 +7,7 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Text;
-using System.Windows.Forms;
+using System.Windows.Forms; 
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
